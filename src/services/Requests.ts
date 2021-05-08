9aebe6483ac6b4cba162c8966be8a0e99edbbeb0
leoops <leoanardopereira519@gmail.com>
2021-05-08 00:23:18 -0300

    Atualização de requisição

--- src/services/Requests.ts
+++ src/services/Requests.ts
@@ -1,9 +1,14 @@
 import axios from './Axios';
 
-export interface RepoProperties {
+export interface RepositoryParams {
   language: string;
   page: number;
   sort: string;
+}
+
+export interface PullRequestParams {
+  creator: string;
+  repository: string;
 }
 
 export interface Repository {
@@ -27,21 +32,8 @@ export interface PullRequest {
   html_url: string;
 }
 
-export interface PullRequestProperties {
-  creator: string;
-  repository: string;
-}
-
-/**
- * @instance
- * @method getAllRepositoriesBy
- * @description Pesquisa de repositórios
- * @param {string} language - Linguagem a ser pesquisada
- * @param {int} page - Numero da pagina
- * @param {string} sort - Parâmetro de ordenação da lista de repositórios
- */
-//  https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1
-export const requestAllRepositoriesBy = (params: RepoProperties) => {
+/* https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1 */
+export const requestAllRepositoriesBy = (params: RepositoryParams) => {
   const { language, page, sort } = params;
   const query = `language:${language}&sort=${sort}&page=${page}`;
   const url = `/search/repositories?q=${query}`;
@@ -49,15 +41,8 @@ export const requestAllRepositoriesBy = (params: RepoProperties) => {
   return axios.get(url);
 };
 
-/**
- * @instance
- * @method GetAllPullResquestsBy
- * @description Busca de pull requests de repositório especifico
- * @param {string} creator - Nome do criador do repositório
- * @param {string} repositor - Nome do repositório
- */
-// URL= <criador>/<repositório>/pulls
-export const requestAllPullRequestsBy = (params: PullRequestProperties) => {
+/* URL= https://api.github.com/repos<criador>/<repositório>/pulls */
+export const requestAllPullRequestsBy = (params: PullRequestParams) => {
   const { creator, repository } = params;
   const url = `/repos/${creator}/${repository}/pulls`;
 
